e30653a79572270902eab03a7847fc4bf5b482b8
codjoelmayer <tmjoelm4@gmail.com>
2022-12-19 12:33:24 +0200

    Product exercise

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ProductComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import ProductComponent from './components/ProductComponent.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductComponent
   }
 }
 </script>
